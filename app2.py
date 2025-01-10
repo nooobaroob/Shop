c389b054516584713d6546f2596a29eba4b8def7
nooobaroob <ngroktcp8080@tutamail.com>
2025-01-11 00:09:09 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -182,4 +182,4 @@ def get_video_formats():
         return jsonify({'error': 'An error occurred on the server'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
